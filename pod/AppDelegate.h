fe558f1d0fba51769de2e2002deb663d59e2ee07
vistatinchao <zouc@szmn.com.cn>
2016-04-26 11:28:10 +0800

    init
    chushihua

---
+++ pod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pod
+//
+//  Created by mac on 16/4/26.
+//  Copyright © 2016年 United Network Services Ltd. of Shenzhen City. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
